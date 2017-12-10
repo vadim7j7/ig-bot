3183c4e2052e4773cdca7c1b0dd4162748639fab
vadim7j7 <vadim7j7@gmail.com>
2017-12-10 23:58:20 +0700

    Change structures for actions

--- bot/src/structures.py
+++ bot/src/structures.py
@@ -1,17 +1,24 @@
 class Credentials(object):
-    email = ''
-    password = ''
+    email: str = None
+    password: str = None
 
 
 class Action(object):
-    action = ''
-    params = {}
-    config = {}
-    thread_name = 'default'
-    pause = {'from': 1000, 'to': 3000}
+    name: str = None
+    allow: bool = True
+    params: dict = {}
+    config: dict = {'pause': {'from': 1000, 'to': 3000}}
+    data: object = None
+
+    def __init__(self, name: str):
+        self.name = name
 
 
 class User(object):
-    id = None
-    credentials = Credentials
-    actions = list
+    id: int = None
+    credentials: Credentials = Credentials()
+    action_get_media: Action = Action(name='get_media')
+    action_like: Action = Action(name='like')
+    action_follow: Action = Action(name='follow')
+    action_unfollow: Action = Action(name='unfollow')
+    action_comment: Action = Action(name='comment')
--- example.py
+++ example.py
@@ -1,21 +1,12 @@
 from bot.launch import Launch
-from bot.src.structures import User, Action
+from bot.src.structures import User
 
 
 def generate_users() -> list:
-    action_like = Action()
-    action_like.action = 'like'
-    action_like.config = {}
-
-    action_follow = Action()
-    action_like.action = 'follow'
-    action_like.config = {}
-
     user = User()
     user.id = 1
     user.credentials.email = 'LOGIN'
     user.credentials.password = 'PASSWORD'
-    user.actions = [action_like, action_follow, ]
 
     return [user, ]
 
